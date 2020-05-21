8f47c839ada4cbf48978b60ce6d646f656c46b42
Rutvik <rutvikpatel.rm@gmail.com>
2020-05-21 18:42:30 -0500

    chnage in meme contract

--- starter_kit/src/contracts/Meme.sol
+++ starter_kit/src/contracts/Meme.sol
@@ -19,20 +19,26 @@ contract Meme{
         string groupHash;
         uint groupVersion;
     }
+    struct postHash{
+        uint postId;
+        string postedEmailId;
+        string postHash;
+        uint uniqueNumber;
+        string date;
+    }
 
     //Write function
   
        mapping(uint=>userHash) public userInformation;
        mapping(uint=>groupHash) public groupInformation;
+       mapping(uint=>postHash) public postInformation;
 
     function addUser(string memory _userEmailId,string memory _userHash,string memory _publicKey) public {
         userCount++;
          userInformation[userCount]= userHash(userCount,_userEmailId,_userHash, _publicKey);
-          
      }
 
     uint public userCount=0;
-
     function changeUserInformation(uint  _userId,string memory _newHash) public {
         uint checkId = _userId;
         string memory newHash=_newHash;
@@ -43,16 +49,30 @@ contract Meme{
             userInformation[_userId].userHash=k;
         }
     }
-
     uint public groupCount=0;
     function createGroup(string memory _groupEmailId,string memory _groupHash, uint  _versionId)  public {
-
         groupCount++;
         groupInformation[groupCount]=groupHash(groupCount,_groupEmailId,_groupHash,_versionId);
     }
+
+  
+       
+    
+
+    uint public postCount=0;
+    function createPost(string memory _postedByEmailId,string memory _postHash, uint _uniquePostId ,string memory _currentDateAndTime)  public {
+        postCount++;
+        postInformation[postCount]=postHash(postCount,_postedByEmailId,_postHash,_uniquePostId,_currentDateAndTime);
+    }
+
+
 
        function getuserCount() public view returns(uint)
     {
         return userCount;
     }
+
+  
+
+
 }
