df5b72156b251667a46b4ad0e778eb0b6d61c1f1
Rutvik <rutvikpatel.rm@gmail.com>
2020-06-29 23:20:52 -0500

    adding new logic to view post

--- starter_kit/src/components/viewPost.js
+++ starter_kit/src/components/viewPost.js
@@ -51,8 +51,12 @@ class viewPost  extends Component{
           privateKey:'',
           originalPostContentForFriendList:'',
           renderConditionFirst:0,
-          editTextArea:true
-
+          editTextArea:true,
+          //
+          firstChecker:0,
+          secondChecker:0,
+          vCondition:0,
+          globalUserData:''
         };       
       }
       async componentWillMount(){
@@ -136,6 +140,7 @@ class viewPost  extends Component{
                 console.log(this.state.PostOwnerFullName);
                 console.log("///******");
                 console.log("found it")
+                this.setState({globalUserData:userData});
                 console.log(userData.postData[j])
                 this.setState({originalPostContentForFriendList:userData.postData[j]})
                 console.log("///******");
@@ -161,17 +166,157 @@ class viewPost  extends Component{
             count++;
             if(count==this.state.groupInformationListFromBlockChain.length){
                 console.log("in if");
-                this.displayData2()
+                this.displayData3()
             }
         })
        }
         
-
         
         // this.setState({currentUserGroupHash:lastestGroupDetailHash.groupHash})
         // this.setState({currentGroupKeyInformation:lastestGroupDetailHash});
 
     }
+
+    displayData3=()=>{
+      console.log("in displaydata 3")
+      console.log("Checking the first conditioon");
+      console.log(this.state.originalPostContentForFriendList);
+      ////
+      for(var i=0;i<this.state.originalPostContentForFriendList.friendList.length;i++){
+        console.log(this.state.originalPostContentForFriendList.friendList[i]);
+          if(this.state.originalPostContentForFriendList.friendList[i].name===this.state.currentUserName){
+            console.log(this.state.originalPostContentForFriendList.friendList[i].name)
+            this.setState({firstChecker:1});
+            break;
+          }
+      }
+      ///
+      this.displayData4();
+    }
+
+    displayData4=()=>{
+      console.log("in display data 4");
+      console.log(this.state.globalUserData);
+      for(var i=0;i<this.state.globalUserData.friend.length;i++){
+        console.log(this.state.globalUserData.friend[i].name);
+        if(this.state.currentUserName==this.state.globalUserData.friend[i].name){ 
+          this.setState({secondChecker:1})
+          break;
+        }
+      }
+      this.displayData5();
+    }
+
+    displayData5=()=>{
+      console.log("in display data 5");
+      if(this.state.firstChecker==0 && this.state.secondChecker==0){
+        console.log("Post should not be shown")
+        console.log("add friend");
+        this.setState({vCondition:0});
+        this.setState({loading:false});
+      }
+      else if(this.state.firstChecker==1 && this.state.secondChecker==0){
+        console.log("You are friend when the post was published");
+        console.log("you are not friend right now");
+        this.setState({vCondition:1});
+      }
+      else if(this.state.firstChecker==0 && this.state.secondChecker==1){
+        console.log("you are able to see the post")
+        this.setState({vCondition:1});
+      }
+      else if(this.state.firstChecker==1 && this.state.secondChecker==1){
+        console.log("you are able to see the post")
+        this.setState({vCondition:1});
+      }
+      else{
+        this.setState({vCondition:0});
+        this.setState({loading:false});
+      }
+      this.displayData6();
+    }
+
+    displayData6=()=>{
+      if(this.state.vCondition==1){
+        console.log("get data infromation");
+        this.displayData7();
+      }
+      else{
+
+      }
+    }
+
+    displayData7=()=>{
+     // postHash
+      ipfs.get("/ipfs/"+this.state.originalPostContentForFriendList.postHash,(error,result)=>{
+        var tempContent=result[0].content;
+        console.log(tempContent);
+        var tempgroupdata=JSON.parse(tempContent);
+        var postData=tempgroupdata;
+        console.log(postData);
+        this.setState({postData:postData.postTextHash});
+        var ipfsLink="https://ipfs.infura.io/ipfs/";
+        var gart=postData.postPicHash;
+        var result1=ipfsLink+gart;
+        this.setState({postImage:result1})
+        // console.log(tempgroupdata);
+        // this.setState({postUserGroupIPFSData:tempgroupdata});
+        //console.log(this.state.postUserPostIPFSData);
+        this.decryptedData2();
+
+      });
+    }
+
+    decryptedData2=()=>{
+      console.log(this.state.PostOwnerData);
+      console.log(this.state.postUserGroupIPFSData);
+      if(this.state.PostOwnerData.image=='yes'){
+        this.setState({imageData:"yes"});
+      }
+      else if(this.state.PostOwnerData.video=='yes'){
+        this.setState({videoData:"yes"});
+      }
+    //  console.log(this.state.postData.postTextHash);
+    //console.log(this.state.postData);
+    ipfs.get("/ipfs/"+this.state.postData,(error,result)=>{
+      var tempContent=result[0].content;
+      var postData=JSON.parse(tempContent);
+      console.log(postData);
+      this.setState({displayData:postData});
+      ///
+      var str = this.state.PostOwnerFullName;
+      var array = str.split(" ");
+      var array1=array[0];
+      var array2=array[1];
+      var array1=array1.substring(0,1);
+      var array2=array2.substring(0,1);
+      var final =array1+array2;
+      console.log(final);
+      //signatureText
+      this.setState({ signatureText:final});
+      this.setState({loading:false});
+    });
+    }
+  // renderData2=()=>{
+  //   ipfs.get("/ipfs/"+this.state.postData,(error,result)=>{
+  //     var tempContent=result[0].content;
+  //     var postData=JSON.parse(tempContent);
+  //     console.log(postData);
+  //     this.setState({displayData:postData});
+  //     ///
+  //     var str = this.state.PostOwnerFullName;
+  //     var array = str.split(" ");
+  //     var array1=array[0];
+  //     var array2=array[1];
+  //     var array1=array1.substring(0,1);
+  //     var array2=array2.substring(0,1);
+  //     var final =array1+array2;
+  //     console.log(final);
+  //     //signatureText
+  //     this.setState({ signatureText:final});
+  //   });
+  // }
+
+
 
     displayData2=()=>{
 
@@ -224,6 +369,7 @@ class viewPost  extends Component{
              // this.decryptedData();
             }
           }
+          console.log(this.state.originalPostContentForFriendList);
           for(var i=0;i<this.state.originalPostContentForFriendList.friendList.length;i++){
             console.log(this.state.originalPostContentForFriendList.friendList[i]);
               if(this.state.originalPostContentForFriendList.friendList[i].name===this.state.currentUserName){
@@ -526,15 +672,105 @@ class viewPost  extends Component{
         }
       }
       addFriend=()=>{
-        //alert("Hello World");
-        // console.log("inside open");
-        // // this.setState({ showModal: true });
-        // console.log(this.state.userInformationListFromBlockChain);
-        // console.log("(((()))");
-        // console.log(this.state.groupInformationListFromBlockChain);
+        console.log("on button Click");        
+        console.log("inside open");
+        console.log(this.state.userInformationListFromBlockChain);
+        console.log("(((()))");
+        console.log(this.state.groupInformationListFromBlockChain);
+        this.friend1();
+        this.friend2();
+         
       }
 
-      
+      friend1=()=>{
+        console.log("adding in friend 1");
+        console.log(this.state.currentUserName);
+        console.log("Current Username");
+        console.log(this.state.currentUserEmailId);
+        console.log(this.state.groupInformationListFromBlockChain);
+        var dataArray=[];
+
+        for(var i=0;i<this.state.groupInformationListFromBlockChain.length;i++){
+          if(this.state.groupInformationListFromBlockChain[i].groupEmailId==this.state.currentUserEmailId){
+            dataArray.push(this.state.groupInformationListFromBlockChain[i]);
+          }
+        }
+  
+        let myMap = new Map();
+        var max=-1;
+        for(var i=0;i<dataArray.length;i++){
+         var value=dataArray[i].groupVersion;
+         console.log(value);
+         value=value.toString();
+         myMap.set(value,dataArray[i]);
+         console.log(value);
+         if(value>max){
+           max=value;
+         }
+        }
+        console.log("lastest group version is");
+        console.log(max);
+        console.log(myMap.get(max));
+        var lastestGroupDetailHash= myMap.get(max);
+        console.log(lastestGroupDetailHash);
+        ///
+        var currentgroupId=lastestGroupDetailHash.groupId;
+        console.log(currentgroupId);
+
+
+        //
+        ipfs.get("/ipfs/"+lastestGroupDetailHash.groupHash,(error,result)=>{
+          //console.log(result[0].path);
+          var content=result[0].content;
+          console.log(content);
+          var userData=JSON.parse(content);
+          console.log(userData);
+      })
+      }
+
+      friend2=()=>{
+        console.log("adding in friend 2"); 
+        console.log(this.state.PostOwnerFullName);
+        var dataArray=[];
+        //
+        for(var i=0;i<this.state.groupInformationListFromBlockChain.length;i++){
+          if(this.state.groupInformationListFromBlockChain[i].groupEmailId==this.state.PostOwnerEmailId){
+            dataArray.push(this.state.groupInformationListFromBlockChain[i]);
+          }
+        }
+  
+        let myMap = new Map();
+        var max=-1;
+        for(var i=0;i<dataArray.length;i++){
+         var value=dataArray[i].groupVersion;
+         console.log(value);
+         value=value.toString();
+         myMap.set(value,dataArray[i]);
+         console.log(value);
+         if(value>max){
+           max=value;
+         }
+        }
+        console.log("lastest group version is");
+        console.log(max);
+        console.log(myMap.get(max));
+        var lastestGroupDetailHash= myMap.get(max);
+        console.log(lastestGroupDetailHash);
+        var currentgroupId=lastestGroupDetailHash.groupId;
+        console.log(currentgroupId);
+
+        ///
+
+        ipfs.get("/ipfs/"+lastestGroupDetailHash.groupHash,(error,result)=>{
+          //console.log(result[0].path);
+          var content=result[0].content;
+          console.log(content);
+          var userData=JSON.parse(content);
+          console.log(userData);
+      })
+      }
+
+
       render(){
         var cardStyle2={
         
@@ -717,6 +953,167 @@ class viewPost  extends Component{
       paddingTop:"30px"
     }
 
+
+
+const renderAuthButton2 =()=>{
+
+  if(this.state.loading=="true"){
+    console.log("in true if")
+    return(
+      <div  style={{textAlign:"center"}}>
+        Loading.....
+      </div>
+    )
+  }
+  else{
+    if(this.state.vCondition==0){
+      return(
+        <div>
+                  <div className="container">
+              <div className="text-center ">
+                  <h4 style={name}>You are not a friend when the post was publish</h4>
+                <hr></hr>
+                <div style={border}>
+                <div  style={cardStyle}>
+                
+                  <div style={info}>
+                      <img style={photo} src='https://www.gstatic.com/tv/thumb/persons/509114/509114_v9_ba.jpg' ></img>
+                      <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
+                    </div>
+                    <h4>Want to see the post add {this.state.PostOwnerFullName} </h4>
+                    <Button variant="primary"  onClick={this.addFriend} >Add Friend</Button>
+                    {/* <button type="button"  onClick={this.addFriend()}   >Click Me!</button> */}
+                  </div>
+                </div>
+              </div>       
+            </div>
+        </div>
+      )
+    }
+    else if(this.state.vCondition==1){
+      if(this.state.imageData=="yes"){
+        return(
+    
+    //       <div className="row">
+    //       <div className="col-2">
+    //           Hello World
+    //        </div>
+    //        <div className="col-8">In second div
+    //         <div style={cardStyle}>
+    //             <div style={card} expand="false">
+    //                <div style={info}>
+    //                <img style={photo} src='https://www.gstatic.com/tv/thumb/persons/509114/509114_v9_ba.jpg' ></img>
+    //                    <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
+    //                    <h3></h3>
+    //                </div>
+    //                <br></br>
+                  
+    //               <p style={{fontSize:"19px",paddingLeft:"7px" }}>{this.state.displayData} <span>{this.state.signatureText}</span></p>
+    //                <hr></hr>
+                  
+    //                    <br></br>
+                     
+    //                <img src={this.state.postImage}  style={{height: "100%",  width:"700px",marginLeft:"150px" }}/>
+    //                <p style={bottomRight}>Bottom Right</p>
+                
+                   
+    //             </div>
+    //        </div> 
+    //        </div>
+    //        <div  className="col-2">
+    //            In third div
+    //        </div>
+    // </div>  
+    
+    <div>
+    {/* <h2>In About Component</h2> */}
+    <div style={border}>
+    <div className="container">
+    <div style={info}>
+    <img style={photo} src='https://www.gstatic.com/tv/thumb/persons/509114/509114_v9_ba.jpg' ></img>
+     <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
+    
+    </div>
+    <hr></hr>
+    <p style={tagLine}>
+    {this.state.displayData}
+     <span style={signature}>{this.state.signatureText}</span>
+    </p>
+    <hr style={{width:"40px",textAlign:"left",marginLeft:"730px",marginTop:"-15px",  position:"relative",borderTop: "7px solid" }}></hr> 
+    </div>
+    
+    <div className="container" style={imageStyle}>
+    <img src={this.state.postImage} style={{height:'700px', width:"100%"}} alt="Notebook" />
+    <div style={text}>
+        <p>{this.state.signatureText}</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    
+        )
+      }
+      else if(this.state.videoData=="yes"){
+        return(
+    
+                   
+                  
+          //
+          <div className="row">
+          <div className="col-2">
+              video Player
+           </div>
+           <div className="col-8">In second div
+            <div style={cardStyle}>
+                <div style={card} expand="false">
+                   <div style={info}>
+                   <img style={photo} src='https://www.gstatic.com/tv/thumb/persons/509114/509114_v9_ba.jpg' ></img>
+                       <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
+                       <h3></h3>
+                   </div>
+                   <br></br>
+                  
+                  {/* <p style={{fontSize:"19px",paddingLeft:"7px" }}>{this.state.displayData} <span>{this.state.signatureText}</span></p> */}
+                  <p style={tagLine}>
+    {this.state.displayData}
+     <span style={signature}>{this.state.signatureText}</span>
+    </p>
+    <hr style={{width:"40px",textAlign:"left",marginLeft:"730px",marginTop:"-15px",  position:"relative",borderTop: "7px solid" }}></hr> 
+                   <hr></hr>
+                  
+                       <br></br>
+                   {/* <img src={this.state.postImage}  style={{height: "100%",  width:"700px",marginLeft:"150px" }}></img> */}
+                   <div style={{height: "100%",  width:"700px",marginLeft:"150px" }}>
+                   <Player
+                        playsInline
+                        poster="/assets/poster.png"
+                        src="https://ipfs.infura.io/ipfs/QmTJcgraP6MR8gamSVgjyDWKeR2naRVzMNmaZnZ7PKQNxY"
+                        />
+                   </div>
+                 
+                   <div class="bottomRight">Bottom Right</div>
+                   <hr></hr>
+                   
+                </div>
+           </div> 
+           </div>
+           <div  className="col-2">
+               In third div
+           </div>
+    </div> 
+    
+          //
+          
+        )
+      }
+      
+    }
+  }
+}
+
+
+
   const renderAuthButton = ()=>{
 
 
@@ -871,7 +1268,7 @@ else{
                     <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
                   </div>
                   <h4>Want to see the post add {this.state.PostOwnerFullName} </h4>
-                  <Button variant="primary"  onClick={this.addFriend()} >Add Friend</Button>
+                  <Button variant="primary"  onClick={this.addFriend} >Add Friend</Button>
                   {/* <button type="button"  onClick={this.addFriend()}   >Click Me!</button> */}
                 </div>
               </div>
@@ -1028,7 +1425,7 @@ else if(this.state.videoData=="yes"){
                     <div style={name}><h4> {this.state.PostOwnerFullName}</h4></div>
                   </div>
                   <h4>Want to see the post add {this.state.PostOwnerFullName} </h4>
-                  <Button variant="primary" onClick={this.addFriend()} >Add Friend</Button>
+                  <Button variant="primary" onClick={this.addFriend} >Add Friend</Button>
                   {/* <button type="button"  onClick={this.addFriend()}   >Click Me!</button> */}
                 
                 </div>
@@ -1106,8 +1503,8 @@ const viewName=()=>{
                </br>
                <br>
                </br>
-               {renderAuthButton()}
-      
+               {/* {renderAuthButton()} */}
+               {renderAuthButton2()}
                     <hr></hr>
                     <div style={{textAlign:"center"}}>
                     
